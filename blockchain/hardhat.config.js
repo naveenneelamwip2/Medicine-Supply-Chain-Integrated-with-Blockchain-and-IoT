41abeb4033ef760304071bfff667dd7101fdee90
Naveen Neelam <naveen@ubuntu.myguest.virtualbox.org>
2024-10-09 00:24:28 +0530

    ganache added

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -10,6 +10,10 @@ module.exports = {
     fontomtest: {
       url: process.env.provider_url,
       accounts: [`0x${process.env.provider_key}`]
+    },    
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
+      accounts: [`0x2f0e73ecae076ed675b3a368a9ebc3738fef8c438b1d491b23c40b95cd343c5d`]
     }
   }
 };
