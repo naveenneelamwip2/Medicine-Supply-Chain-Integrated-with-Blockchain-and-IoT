f9fb1f49c97087086d3fca398674cddf85243568
Naveen Neelam <naveen@ubuntu.myguest.virtualbox.org>
2024-10-10 16:03:51 +0530

    medicines all controllers fun complected

--- backend/src/medicine/medicine.service.ts
+++ backend/src/medicine/medicine.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { EthersService } from '../common/ethers.service';
 import { IpfsService } from '../common/ipfs.service';
 import * as crypto from 'crypto';
+import { ethers } from 'ethers';
+import { medicineContractAbi } from 'src/common/contract_abi';
 
 @Injectable()
 export class MedicineService {
+  private ifaceMedicineContract = new ethers.Interface(medicineContractAbi);
+
   constructor(
     private readonly ethersService: EthersService,
     private readonly ipfsService: IpfsService,
@@ -17,35 +21,44 @@ export class MedicineService {
     medicineData.medicineId = crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 
     const userCid = await this.ethersService.getUserCid(email);
+    if(!userCid) return { status: 401, message: 'User not found in blockchain', data: email };
     const medicineCid = await this.ipfsService.encryptAndStore(medicineData);
-    await this.ethersService.addMedicine(userCid, medicineData.medicineId, medicineCid);
-    return { message: 'Medicine added successfully' };
+    let tx = await this.ethersService.addMedicine(userCid, medicineData.medicineId, medicineCid);
+    return { status:201, message: 'Medicine added successfully', data: {tx, medicineData, cid: medicineCid} };
   }
 
   async updateMedicineById(email: string, updateData) {
+    updateData.lastUpdatedDate = new Date().toString();
     const userCid = await this.ethersService.getUserCid(email);
-    const oldMedicineDoc = await this.getMedicineById(updateData.medicineId, userCid);
-    Object.assign(oldMedicineDoc, updateData);
-    const newMedicineCid = await this.ipfsService.encryptAndStore(oldMedicineDoc);
-    await this.ethersService.updateMedicine(userCid, updateData.medicineId, newMedicineCid);
-    return { message: 'Medicine updated successfully' };
+    const MedicineDoc = await this.getMedicineById(userCid, updateData.medicineId);
+    Object.assign(MedicineDoc, updateData);
+    const newMedicineCid = await this.ipfsService.encryptAndStore(MedicineDoc);
+    const tx = await this.ethersService.updateMedicine(userCid, updateData.medicineId, newMedicineCid);
+    return { status:201, message: 'Medicine updated successfully', data: {tx, MedicineDoc, cid: newMedicineCid} };
   }
 
   async getAllMedicinesByEmail(email: string) {
     const userCid = await this.ethersService.getUserCid(email);
     const medicineEvents = await this.ethersService.getAllUserMedicineEvents(userCid);
-    let medicines = []
-    medicineEvents.forEach(async event => {
-        let medicineDoc = this.getMedicineById(userCid, event.args.medicineId)
-        medicines.push(medicineDoc)
-    });
-
-    return medicines
+    const medicines = await this.decodeEventAndRetrieveDocs(userCid, medicineEvents)
+    return { status:201, message: 'Retrieved all user medicines successfully', data: medicines };
   }
 
   private async getMedicineById(userCid:string, medicineId: string) {
     const medicineCids = await this.ethersService.getMedicineHistory(userCid, medicineId);
     const medicineDoc = await this.ipfsService.retrieveAndDecrypt(medicineCids[0]);
     return medicineDoc
+  }
+
+  private async decodeEventAndRetrieveDocs(userCid, medicineEvents) {  
+    let medicines = [];
+
+    await Promise.all(medicineEvents.map(async event => {
+        const evtargs = this.ifaceMedicineContract.decodeEventLog("MedicineEvt", event.data);
+        let medicineDoc = await this.getMedicineById(userCid, evtargs[2]);
+        medicines.push(medicineDoc);
+    }))
+
+    return medicines
   }
 }
